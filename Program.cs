627419b58d2cd74032a952e47cdbf049856e7b98
Claudio-DiGiovanni <digioclaudio97@gmail.com>
2023-03-27 11:34:41 +0200

    milestone 3

--- Program.cs
+++ Program.cs
@@ -1,5 +1,41 @@
 ﻿
-menu();
+var evento1 = new Evento("Ciao", "15/07/2023", 150);
+var evento2 = new Evento("Ciao", "15/07/2023", 150);
+var evento3 = new Evento("Ciao", "15/07/2023", 150);
+var evento4 = new Evento("Ciao", "15/07/2023", 150);
+var evento5 = new Evento("Ciao", "15/07/2023", 150);
+var evento6 = new Evento("Ciao", "15/07/2023", 150);
+var evento7 = new Evento("Ciao", "15/07/2023", 150);
+var evento8 = new Evento("Ciao", "15/07/2023", 150);
+var evento9 = new Evento("Ciao", "15/07/2023", 150);
+var evento10 = new Evento("Ciao", "15/07/2023", 150);
+var evento11 = new Evento("Ciao", "15/07/2023", 150);
+var evento12 = new Evento("Ciao", "15/07/2023", 150);
+var evento13 = new Evento("Ciao", "15/07/2023", 150);
+var evento14 = new Evento("Ciao", "15/07/2023", 150);
+var evento15= new Evento("Ciao", "15/07/2023", 150);
+var evento16 = new Evento("Ciao", "15/07/2023", 150);
+
+var programmazione = new ProgrammaEventi("Programmazione");
+
+programmazione.AggiungiEvento(evento1);
+programmazione.AggiungiEvento(evento2);
+programmazione.AggiungiEvento(evento3);
+programmazione.AggiungiEvento(evento4);
+programmazione.AggiungiEvento(evento5);
+programmazione.AggiungiEvento(evento6);
+programmazione.AggiungiEvento(evento7);
+programmazione.AggiungiEvento(evento8);
+programmazione.AggiungiEvento(evento9);
+programmazione.AggiungiEvento(evento10);
+programmazione.AggiungiEvento(evento11);
+programmazione.AggiungiEvento(evento12);
+programmazione.AggiungiEvento(evento13);
+programmazione.AggiungiEvento(evento14);
+programmazione.AggiungiEvento(evento15);
+programmazione.AggiungiEvento(evento16);
+
+Console.WriteLine(programmazione.Programmazione());
 void menu()
 {
     Console.WriteLine("Inserisci un nuovo evento: ");
---
+++ ProgrammaEventi.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class ProgrammaEventi
+{
+    string titolo;
+    List<Evento> eventi;
+
+    public ProgrammaEventi(string titolo)
+    {
+        this.titolo = titolo;
+        this.eventi = new List<Evento>();
+    }
+    public void AggiungiEvento(Evento evento)
+    {
+        eventi.Add(evento);
+    }
+
+    public List<Evento> ListaPerData(string data)
+    {
+        var eventiPerData = new List<Evento>();
+        var dataDaCercare = DateOnly.ParseExact(data, "dd/MM/yyyy", null);
+
+        foreach (var evento in eventi)
+        {
+            if (evento.Data == dataDaCercare)
+            {
+                eventiPerData.Add(evento);
+            }
+        }
+
+        return eventiPerData;
+    }
+
+    public int NumeroEventi()
+    {
+        return eventi.Count;
+    }
+
+    public void SvuotaListaEventi()
+    {
+        eventi.Clear();
+    }
+
+    public string Programmazione()
+    {
+        var nl = Environment.NewLine;
+        var programmazione = "Gestore Eventi:" + nl;
+        foreach ( var evento in eventi)
+        {
+            programmazione += evento.ToString() + nl;
+        }
+        return programmazione;
+      
+    }
+}
+
